d799f2aa6561ef79b100c0d8bd8dbd8bd58731e1
Adwait Bhope <adwaitbhope@gmail.com>
2019-03-07 17:06:18 +0530

    Added Nav Drawer options and icons

--- app/src/main/java/com/piedpiper/epimaps/MainActivity.java
+++ app/src/main/java/com/piedpiper/epimaps/MainActivity.java
@@ -81,17 +81,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.main_nav_login) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.main_nav_subscribe) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.main_nav_feedback) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.main_nav_about) {
 
         }
 
