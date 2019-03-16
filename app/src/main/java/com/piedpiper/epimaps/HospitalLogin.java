4e6e46fd767747cf6dee11a4c14adcc104fb8c73
vk2607 <vinodkamat98@gmail.com>
2019-03-16 19:52:10 +0530

    Mapssy

--- app/src/main/java/com/piedpiper/epimaps/HospitalLogin.java
+++ app/src/main/java/com/piedpiper/epimaps/HospitalLogin.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HospitalLogin extends AppCompatActivity {
 
     TextView registerHospital;
+    Button loginButton;
+//    String email,password,name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,8 @@ public class HospitalLogin extends AppCompatActivity {
         setContentView(R.layout.activity_hospital_login);
 
         registerHospital=(TextView)findViewById(R.id.register_text);
+        loginButton=(Button)findViewById(R.id.signin_button);
+
 
         registerHospital.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +28,14 @@ public class HospitalLogin extends AppCompatActivity {
                 startActivity(new Intent(HospitalLogin.this,HospitalRegister.class));
             }
         });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HospitalLogin.this,HospitalHomeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
