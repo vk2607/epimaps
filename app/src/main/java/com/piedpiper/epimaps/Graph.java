e26765b62f497c783a68d65aa59412f338312cee
prashantbhandari1999 <prashantbhandari1999@gmail.com>
2019-03-16 23:40:04 +0530

    Bar Graph  added with click event

--- app/src/main/java/com/piedpiper/epimaps/Graph.java
+++ app/src/main/java/com/piedpiper/epimaps/Graph.java
@@ -3,22 +3,32 @@ package com.piedpiper.epimaps;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class Graph extends AppCompatActivity {
+public class Graph extends AppCompatActivity  {
 
     BarChart barChart;
+    LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,6 @@ public class Graph extends AppCompatActivity {
         setContentView(R.layout.activity_graph);
 
         barChart = (BarChart) findViewById(R.id.bargraph);
-
         barChart.setDrawBarShadow(false);
         barChart.setDrawValueAboveBar(false);
 //        barChart.set
@@ -57,10 +66,47 @@ public class Graph extends AppCompatActivity {
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new MyAxis(months));
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        barChart.setOnChartValueSelectedListener( new OnChartValueSelectedListener() {
+            public void onValueSelected(Entry e, Highlight h) {
+                lineChart=(LineChart)findViewById(R.id.linegraph);
+                lineChart.setVisibility(View.GONE);
+                setData(40,60);
+                lineChart.animateX(1000);
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+
 //        xAxis.setGranularity(1f);
 //        xAxis.setGranularityEnabled(true);
 //        xAxis.setCenterAxisLabels(true);
 //        xAxis.setAxisMinimum(1);
+
+
+//        lineChart=(LineChart)findViewById(R.id.linegraph);
+//        lineChart.setVisibility(View.GONE);
+//        setData(40,60);
+//        lineChart.animateX(1000);
+    }
+    private void setData(int count,int range){
+        YAxis yAxisRight = lineChart.getAxisRight();
+        yAxisRight.setEnabled(false);
+        ArrayList<Entry>Yvalue=new ArrayList<>();
+        for(int i=0;i<count;i++){
+            float val=(float)(Math.random()*range)+250;
+            Yvalue.add(new Entry(i,val));
+        }
+        LineDataSet set=new LineDataSet(Yvalue,"Data Set");
+
+        LineData data=new LineData(set);
+        lineChart.setData(data);
+        lineChart.setVisibility(View.VISIBLE);
+
+
     }
 
     public class MyAxis implements IAxisValueFormatter {
