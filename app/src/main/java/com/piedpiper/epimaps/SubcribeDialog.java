f23f92705648264e474c0ac5c797284776d2be4b
prashantbhandari1999 <prashantbhandari1999@gmail.com>
2019-03-16 15:24:35 +0530

    Subscribe Dialog & cancel added

--- app/src/main/java/com/piedpiper/epimaps/SubcribeDialog.java
+++ app/src/main/java/com/piedpiper/epimaps/SubcribeDialog.java
@@ -12,56 +12,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class SubcribeDialog extends DialogFragment {
-
     private View view;
-    TextView email,city;
-    Button cancel,ok,location;
+    TextView email, city;
+    Button cancel, ok, location;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
+        super.onCreate(savedInstanceState);
         LayoutInflater inflater = getActivity().getLayoutInflater();
         view = inflater.inflate(R.layout.subscribe_dialog, null);
-
-        Toast.makeText(getActivity(), "Oncreatedialog", Toast.LENGTH_SHORT).show();
-
-        email=(TextView)view.findViewById(R.id.user_email);
-        city=(TextView)view.findViewById(R.id.location_text);
-
-        cancel=(Button)view.findViewById(R.id.cancel_btn);
-        ok=(Button)view.findViewById(R.id.ok_btn);
-        location=(Button)view.findViewById(R.id.location_btn);
-
-        location.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Choose your Location", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Thanks for the subscription", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        return super.onCreateDialog(savedInstanceState);
     }
+
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        return super.onCreateDialog(savedInstanceState);
+//    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Toast.makeText(getActivity(), "Oncreateview", Toast.LENGTH_SHORT).show();
-//        View v = inflater.inflate(R.layout.subscribe_dialog, container, false);
-        email=(TextView)view.findViewById(R.id.user_email);
-        city=(TextView)view.findViewById(R.id.location_text);
+        email = (TextView) view.findViewById(R.id.user_email);
+        city = (TextView) view.findViewById(R.id.location_text);
 
-        cancel=(Button)view.findViewById(R.id.cancel_btn);
-        ok=(Button)view.findViewById(R.id.ok_btn);
-        location=(Button)view.findViewById(R.id.location_btn);
+        cancel = (Button) view.findViewById(R.id.cancel_btn);
+        ok = (Button) view.findViewById(R.id.ok_btn);
+        location = (Button) view.findViewById(R.id.location_btn);
 
         location.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +52,13 @@ public class SubcribeDialog extends DialogFragment {
                 Toast.makeText(getActivity(), "Thanks for the subscription", Toast.LENGTH_SHORT).show();
             }
         });
-        return inflater.inflate(R.layout.subscribe_dialog, null);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+
+            }
+        });
+        return view;
     }
 }
