7c1050bed07aa0f68b126c2be31723432e3fbfc9
vk2607 <vinodkamat98@gmail.com>
2019-03-16 17:39:26 +0530

    Maps

--- app/src/main/java/com/piedpiper/epimaps/MainActivity.java
+++ app/src/main/java/com/piedpiper/epimaps/MainActivity.java
@@ -1,8 +1,23 @@
 package com.piedpiper.epimaps;
 
+import android.Manifest;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.content.Context;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,17 +27,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.graphics.*;
+import android.graphics.Paint.Style;
+import android.graphics.Region.Op;
+import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    @Override
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends FragmentActivity
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private GoogleMap mMap;
+    private LocationManager mLocationManager;
+    private double latitude;
+    private double longitude;
+    private EditText locationEditText;
+    private Geocoder geocoder;
+    private List<Address> addresses;
+//    final Geocoder geocoder=new Geocoder(this);
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        locationEditText = (EditText) findViewById(R.id.locationname_edittext);
 //        toolbar.setTitle("Map");
-        setSupportActionBar(toolbar);
+//        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +88,34 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+
+            location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+        }
+
+
+//        fragmentManager = getFragmentManager();
+//        fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.commit();
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -95,4 +170,121 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    //    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        locationEditText.setText(addresses.get(0).getSubLocality() + "," + addresses.get(0).getPostalCode());
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Location"));
+                try {
+
+                    addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+//                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                String locality=addresses.get(0).getLocality();
+                String postalcode=addresses.get(0).getPostalCode();
+                if (locality!= null &&  postalcode!= null) {
+                    locationEditText.setText(addresses.get(0).getLocality() + ", " + addresses.get(0).getPostalCode());
+                }
+                else if(locality!=null){
+                    locationEditText.setText(addresses.get(0).getPostalCode());
+                }
+                else {
+                    locationEditText.setText("Location not known ,please type pincode");
+                }
+
+            }
+        });
+        highlight();
+
+
+        // Try to obtain the map from the SupportMapFragment.
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    public void highlight() {
+//        CircleOptions circleOptions = new CircleOptions()
+//                .center(new LatLng(latitude, longitude))
+//                .radius(100)
+//                .fillColor(Color.RED);
+//
+//        Circle circle = mMap.addCircle(circleOptions);
+//        mMap.an
+    }
+
+    public void getPosition() {
+
+    }
+
+    //    private void drawMarker(Location location) {
+//        if (mMap != null) {
+//            mMap.clear();
+//            LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
+//            mMap.addMarker(new MarkerOptions()
+//                    .position(gps)
+//                    .title("Current Position"));
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 12));
+//        }
+//
+//    }
+//    public int getZoomLevel(Circle circle) {
+//        int zoomLevel = 11;
+//        if (circle != null) {
+//            double radius = circle.getRadius() + circle.getRadius() / 2;
+//            double scale = radius / 500;
+//            zoomLevel = (int) (16 - Math.log(scale) / Math.log(2));
+//        }
+//        return zoomLevel;
+//    }
+//
+
 }
